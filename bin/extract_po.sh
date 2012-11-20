53edba3bcceffe68e111d9b143cb3e870f438bc8
Niv Sardi <xaiki@evilgiggle.com>
2012-11-20 09:15:04 -0300

    fix typo
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- bin/extract_po.sh
+++ bin/extract_po.sh
@@ -9,7 +9,7 @@
 xgettext --keyword=_ -L Perl --output-dir=locale/templates/LC_MESSAGES --from-code=utf-8 --output=messages.pot\
  `find routes -name '*.js' | grep -v 'i18n.js'`
 xgettext -j --keyword=_ -L Python --output-dir=locale/templates/LC_MESSAGES --from-code=utf-8 --output=messages.pot `find views -name '*.jade'`
-xgettext -j --keyword=_ -L Python --output-dir=locale/templates/LC_MESSAGES --from-code=utf-8 --output=messages.pot `find modelse -name '*.js'`
+xgettext -j --keyword=_ -L Python --output-dir=locale/templates/LC_MESSAGES --from-code=utf-8 --output=messages.pot `find models -name '*.js'`
 
 # i18n-abide supports client-side gettext too. Usually you won't need this, unless your doing some
 # fancy new fangled webapp.
