755d5e106e32d71c39d8e8dc520ff9225e37dc17
Niv Sardi <xaiki@evilgiggle.com>
2012-11-20 21:29:15 -0300

    debug: add a fake media every 4s
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- routes/media.js
+++ routes/media.js
@@ -91,13 +91,10 @@ exports.deleteMedia = function(req, res) {
 // Populate database with sample data -- Only used once: the first time the application is started.
 // You'd typically not find this code in a real-life app, since the database would already exist.
 var populateDB = function() {
+    setInterval(function () {_addMedia ({ file: 'test' + Date.now(), _id: Date.now()})},
+                4*1000);
 
-(function(){
-    for (var i = 0; i < 3; i++) 
-        mediaList.add({ file: 'test' + i, _id: 'test' + i });
-
-})();
-
+    return;
 // FIXME: TODO
     var ffmpeg  = require('fluent-ffmpeg')
     , walk      = require('walk')
