614b18db3e43b973ca9d70831a55a5fcccc79575
Niv Sardi <xaiki@evilgiggle.com>
2012-12-02 13:25:09 -0300

    new Models: [[[[[Pieces]:Block]:Lists]:Universe]
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- models/Media.js
+++ models/Media.js
@@ -64,6 +64,74 @@ Media.Collection = BackboneIO.Collection.extend({
     },
 });
 
+
+Media.Piece = Media.Model.extend ({
+    urlRoot: 'piece',
+});
+
+Media.Block = Media.Collection.extend ({
+    model: Media.Piece,
+    url: 'piece',
+    set_index: function (model, index) {
+        console.log ('BLOCK set_index');
+        console.trace();
+        var id = this._get_id(model);
+        this._set_id (model, id.split('-')[0] + '-' + index);
+
+        return Media.Collection.prototype.set_index.call (this, model, index);
+    },
+});
+
+Media.List = Media.Model.extend ({
+    urlRoot: 'list',
+    initialize: function () {
+        var models = this.get('models')
+        var col    = this.get('collection')
+        console.log ("initing media list", models, col);
+        if (!col || col instanceof Array) {
+            col = new Media.Block(models, {connectable: true});
+            console.log ('col is array ! recreating as collection', col);
+        }
+
+        var self = this;
+        col.wrapper = this;
+
+        col.bind('all', function (a, b) {
+            console.log ('got a change in the force', a, b);
+            var models = self.get('collection').models;
+            self.set({models: models});
+            self.updateMD5(models);
+            console.log ('-----got a change in the force', a, b);
+        }, this);
+
+        this.set ({collection: col, models: col.models});
+        Media.Model.prototype.initialize.call (this);
+    },
+    updateMD5: function (models) {
+        if (server)
+            return;
+        var spark = new SparkMD5();
+        spark.append (this.get('name') || 'NULL');
+
+        console.log ('(MODEL) about to calculate MD5 for', models);
+
+        models.forEach(function (e) {spark.append(e);});
+        this.set_id(spark.end());
+    },
+
+    defaults: {
+        collection: null,
+        models: [],
+        name: null,
+        hash: null,
+        fixed: false,
+        pos: 0,
+    },
+});
+
+Media.Universe = Media.Collection.extend ({
+    url: 'list',
+    model: Media.List,
 });
 
 Media.Piece = Media.Model.extend({
