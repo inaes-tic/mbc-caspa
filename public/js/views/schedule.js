d3f5c56fd09fb66fc4bbd373cac085a904282cdc
Niv Sardi <xaiki@evilgiggle.com>
2012-12-03 17:46:07 -0300

    better looks, WALLKINTUN SALE EL 7D
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- public/js/views/schedule.js
+++ public/js/views/schedule.js
@@ -60,49 +60,11 @@ window.ScheduleView = Backbone.View.extend({
 	    defaultView: 'agendaWeek',
 	    events: [
 		{
-		    title: 'All Day Event',
-		    start: new Date(y, m, 1)
+		    title: 'WALLKINTUN SALE AL AIRE !',
+		    start: new Date(2012, 11, 7, 22, 0),
+		    end:   new Date(2012, 11, 8, 6, 0),
+                    allDay: false,
 		},
-		{
-		    title: 'Long Event',
-		    start: new Date(y, m, d-5),
-		    end: new Date(y, m, d-2)
-		},
-		{
-		    id: 999,
-		    title: 'Repeating Event',
-		    start: new Date(y, m, d-3, 16, 0),
-		    allDay: false
-		},
-		{
-		    id: 999,
-		    title: 'Repeating Event',
-		    start: new Date(y, m, d+4, 16, 0),
-		    allDay: false
-		},
-		{
-		    title: 'Meeting',
-		    start: new Date(y, m, d, 10, 30),
-		    allDay: false
-		},
-		{
-		    title: 'Lunch',
-		    start: new Date(y, m, d, 12, 0),
-		    end: new Date(y, m, d, 14, 0),
-		    allDay: false
-		},
-		{
-		    title: 'Birthday Party',
-					start: new Date(y, m, d+1, 19, 0),
-		    end: new Date(y, m, d+1, 22, 30),
-		    allDay: false
-		},
-		{
-		    title: 'Click for Google',
-		    start: new Date(y, m, 28),
-		    end: new Date(y, m, 29),
-		    url: 'http://google.com/'
-		}
 	    ]
         });
 
