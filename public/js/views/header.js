a0773ec527a03cfe5949abbf4b6a33c074fd1895
Niv Sardi <xaiki@evilgiggle.com>
2013-01-29 16:52:51 -0300

    update time and menu
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- public/js/views/header.js
+++ public/js/views/header.js
@@ -1,14 +1,14 @@
 window.HeaderView = Backbone.View.extend({
     initialize: function () {
-        this.render();
+            $(this.el).html(template.header(this.model.toJSON()));
+            setInterval(function () {
+                    $('#time', this.el).html(moment.utc().format('hh:mm:ss'))
+                    }, 200);
+
     },
 
-    render: function () {
-        $(this.el).html(template.header(this.model.toJSON()));
-        return this;
-    },
     selectMenuItem: function (menuItem) {
-        $('.nav li').removeClass('active');
+        $('#nav li').removeClass('active');
         if (menuItem) {
             $('.' + menuItem).addClass('active');
         }
