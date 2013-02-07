d372f8ddb042220e3e8b37fd2280f7d127cab2c0
Niv Sardi <xaiki@evilgiggle.com>
2013-02-07 13:48:22 -0300

    medaedit should be gone loong ago.
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- public/js/views/medaedit.js
+++
@@ -1,9 +0,0 @@
-window.EditView = Backbone.View.extend({
-    initialize: function () {
-        this.render();
-    },
-    render: function () {
-        $(this.el).html(template.mediaedit());
-        return this;
-    },
-});
