c600ea35a3e317af5a38ce46360369482549b059
Niv Sardi <xaiki@evilgiggle.com>
2013-02-07 14:26:31 -0300

    mubsub.test.js: rename and use defaults
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- mubsub.test.js
+++ mubsub.test.js
@@ -1,9 +1,9 @@
 var db = require('mongoskin').db('localhost:27017/mediadb?auto_reconnect', {safe:true});
 var mubsub = require ('mubsub');
 var client = mubsub (db);
-var schedchannel = client.channel('messages', { size: 10000000, max: 50000 });
+var channel = client.channel('messages');
 
-schedchannel.subscribe ({}, function (sched) {
+channel.subscribe ({}, function (sched) {
         console.log ("got message:", sched);
 });
 
