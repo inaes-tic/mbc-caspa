4466e3bfb3cca6f1d5db102b8747fbb965ebbdf8
Adrian Pardini <publico@tangopardo.com.ar>
2013-03-16 20:02:30 -0300

    draft of knockback based view to edit media details

--- public/js/views/mediadetails.js
+++ public/js/views/mediadetails.js
@@ -1,4 +1,25 @@
-window.MediaView = Backbone.View.extend({
+window.MediaView = function (options) {
+    var model = options['model'];
+    var el = $('#content');
+
+    el.html(template.mediaview(model.toJSON()));
+    console.log('MediaView 2');
+
+    view_model = kb.viewModel(model);
+
+    view_model.save = function (viewmodel) {
+        console.log('MV save()');
+        if (viewmodel) {
+            console.log(viewmodel);
+            viewmodel.model().save();
+        }
+    };
+
+    ko.applyBindings(view_model, el[0]);
+
+}
+
+window.MediaView_orig = Backbone.View.extend({
     el: $("#content"),
     initialize: function () {
         console.log ("rendering media");
