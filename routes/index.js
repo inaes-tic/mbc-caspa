c243818bf58f6ef50213caa70129b5daf72e10e1
Adrian Pardini <github@tangopardo.com.ar>
2013-03-27 17:44:06 -0300

    fix typo in routes/index.js
    Signed-off-by: Adrian Pardini <github@tangopardo.com.ar>

--- routes/index.js
+++ routes/index.js
@@ -67,7 +67,7 @@ module.exports = function(app) {
         path.join(lib_dir, 'arvo.js'),
         path.join(lib_dir, 'sprintf/sprintf.js'),
         path.join(lib_dir, 'stickyPanel/jquery.stickyPanel.js'),
-        path.join(lib_dir, 'airtime/common.js')
+        path.join(lib_dir, 'airtime/common.js'),
         require.resolve('knockout-client/knockout.js'),
         require.resolve('knockback/knockback-core.js'),
         path.join(lib_dir, 'knockout-sortable/build/knockout-sortable.js'),
