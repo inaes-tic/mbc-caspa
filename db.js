5c6b8af6900809b1c9594b7cae9e5dddc813f556
Tomas Neme <lacrymology@gmail.com>
2013-04-05 15:49:11 -0300

    use mbc-common's db initialization script
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- db.js
+++
@@ -1,6 +0,0 @@
-var db;
-exports.db = function() {
-    var conf = require('config').MediaDB;
-    db = require('mongoskin').db(conf.dbHost + ':' + conf.dbPort + '/' + conf.dbName + '?auto_reconnect', {safe:true});
-    return db;
-}
