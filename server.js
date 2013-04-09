601312ec9afa51ad75e55acd57e875683b65a61f
Tomas Neme <lacrymology@gmail.com>
2013-04-09 19:26:54 -0300

    store status in database
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- server.js
+++ server.js
@@ -135,6 +135,8 @@ channel.subscribe({backend: 'mostoStatus'}, function(msg) {
         });
     }
 });
+statusbackend.use(backboneio.middleware.mongoStore(db, 'status'));
+
 _([mediabackend, listbackend]).each (debug_backend);
 
 backboneio.listen(app.listen(app.get('port'), function(){
