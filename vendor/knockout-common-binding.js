fc9ac31ece905470be7584b400b76cfd76bc2b0d
José Luis Di Biase <josx@interorganic.com.ar>
2013-06-29 14:31:52 -0300

    Ko binding: save on enter
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- vendor/knockout-common-binding.js
+++ vendor/knockout-common-binding.js
@@ -1,15 +1,15 @@
 ko.bindingHandlers.addOnEnter = {
-    init: function(element, valueAccessor) {
+    init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
         var value = valueAccessor();
         $(element).keypress(function(event) {
             keyCode = (event.which ? event.which : event.keyCode);
-                if (keyCode === 13) {
-                    value.call(this);
-                    return false;
-                }
-                return true;
-            });
-        },
-        update: function(element, valueAccesor) {
+            if (keyCode === 13) {
+                value.call(this, viewModel);
+                return false;
+            }
+            return true;
+        });
+    },
+    update: function(element, valueAccesor, allBindingsAccessor, viewModel) {
     }
 };
