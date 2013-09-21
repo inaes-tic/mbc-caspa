inaes-tic/mbc-caspa

commit df91201173feb4d3e27f1b5a7721c4d9ee1d2df3

Replacing hash with html5 pushstate for urls
Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

server.js
@@ -50,7 +50,6 @@ app.configure(function () {
     app.use(express.methodOverride());
     app.use(express.cookieParser('your secret here'));
     app.use(express.session());
-    app.use(app.router);
     app.use(require('less-middleware')({
         src:  conf.Dirs.styles,
         dest: conf.Dirs.pub,
@@ -59,6 +58,7 @@ app.configure(function () {
     app.use(express.static(conf.Dirs.pub, {maxAge: maxage}));
     app.use('/models', express.static(conf.Dirs.models, {maxAge: maxage}));
     app.use('/lib',    express.static(conf.Dirs.vendor, {maxAge: maxage}));
+    app.use(app.router);
 });
 
 app.configure('development', function(){
@@ -74,7 +74,7 @@ app.configure('production', function(){
 });
 
 var appModel = require('./routes')(app);
-var media = require('./routes/media')(app);
+//var media = require('./routes/media')(app);
 
 function debug_backend (backend) {
         console.log ('Debugging backend: ', backend);
@@ -241,7 +241,7 @@ var utils = require('./utils');
 if (process.env.MBC_SCRAPE) {
     setTimeout(function () {
         utils.scrape_files (conf.Dirs.scrape, function (model) {
-            db.collection('medias').insert(model, {safe:true}, function(err, result) {
+            db.collection(collection.Medias).insert(model, {safe:true}, function(err, result) {
                 if (err) {
                     console.error ('error','An error has occurred' + err);
                 } else {

public/js/main.js
@@ -159,5 +159,18 @@ $.ajax({
 
 appCollection.fetch({success: function() {
     app = new AppRouter();
-    Backbone.history.start();
+    Backbone.history.start({pushState:true});
 }});
+
+$(document).on("click", "a[href^='/']", function(event) {
+    var href, url;
+    href = $(event.currentTarget).attr('href');
+    if (!event.altKey && !event.ctrlKey && !event.metaKey && !event.shiftKey) {
+        event.preventDefault();
+        console.log("---------------------------->", href);
+        url = href.replace(/^\//, '').replace('\#\!\/', '');
+        console.log("---------------------------->", url);
+        app.navigate(url, {trigger: true});
+        return false;
+    }
+});

routes/index.js
@@ -16,17 +16,6 @@ module.exports = function(app) {
         });
     });
 
-    /*
-     * GET home page.
-     */
-    app.get('/',  function(req, res) {
-        res.render('index', { name: conf.Branding.name, description: conf.Branding.description });
-    });
-
-    app.get('/app/:id', function (req, res) {
-
-    });
-
     app.get('/po/:id', function (req, res) {
         var lang = req.params.id;
         var locale = i18n.localeFrom(lang);
@@ -192,6 +181,10 @@ module.exports = function(app) {
 
     // serve using express
     app.get('/js/templates.js', folio.serve(templateJs));
+
+    app.get('*',  function(req, res) {
+        res.render('index', { name: conf.Branding.name, description: conf.Branding.description });
+    });
 
     return appCollection;
 }

views/index.jade
@@ -1,14 +1,16 @@
 extends layout
 
 block content
+  - var mbc_url= 'https://github.com/inaes-tic/mbc-playout/';
+
   .home-hero
     h1= gettext('Welcome to ') + name
     br
     div(style="opacity: .9;")
-      a.btn.btn-large(href="#media")
+      a.btn.btn-large(href="/media")
         img.pull-left(src="img/media.png", style="margin-right:6px;").
         p= gettext('Start Browsing\nMedia Cellar')
-      a.btn.btn-large(href="#")
+      a.btn.btn-large(href=mbc_url)
         img.pull-left(src="img/github.png", style="margin-right:6px").
         p= gettext('View Project\non Github')
   .bs-docs-social
@@ -17,4 +19,4 @@ block content
           li.follow-btn
             a.twitter-follow-button(href="https://twitter.com/mbcplayout",data-link-color="#0069D6",data-show-count="true")= gettext('Follow') + ' @mbcplayout'
           li.tweet-btn
-            a.twitter-share-button(href="https://twitter.com/share",data-url="http://localhost:3000",data-count="horizontal",data-via="mbcplayout")= gettext('Tweet')
+            a.twitter-share-button(href="https://twitter.com/share",data-url=mbc_url,data-count="horizontal",data-via="mbcplayout")= gettext('Tweet')

views/templates/header.jade
@@ -21,26 +21,26 @@ ul#nav
         li.top.list-menu
           i#toggle-header.icon-resize-vertical.icon-white
         li.top.list-menu
-          a.top_link(href="#media")
+          a.top_link(href="/media")
             i.icon-list.icon-white
             span=i18n.gettext('Browse Medias')
         li.top.edit-menu
-          a.top_link(href="#media/edit")
+          a.top_link(href="/media/edit")
             i.icon-edit.icon-white
             span= i18n.gettext('Edit Playlists')
         //li.top.add-menu
-          a.top_link(href="#media/add")
+          a.top_link(href="/media/add")
             i.icon-plus.icon-white
             span= i18n.gettext('Add Medias')
         li.top.playout-menu
-          a.top_link(href="#playout")
+          a.top_link(href="/playout")
             i.icon-tasks.icon-white
             span= i18n.gettext('Playout')
         li.top.schedule-menu
-          a.top_link(href="#schedule")
+          a.top_link(href="/schedule")
             i.icon-globe.icon-white
             span= i18n.gettext('Schedule')
         li.top.conf-menu
-          a.top_link(href="#admin")
+          a.top_link(href="/admin")
             i.icon-wrench.icon-white
             span= i18n.gettext('Configure')

views/templates/medialist.jade
@@ -74,9 +74,9 @@ block content
                 tr(data-bind=inndbind)
                   td.center
                     - if (!is_playlist)
-                      - var target = "attr:{href: '#media/'+checksum().split('-')[0]}";
+                      - var target = "attr:{href: '/media/'+checksum().split('-')[0]}";
                     a.plain(data-bind=target)
-                      img(width="150",height="100",data-bind="attr:{ src: checksum() ?  'sc/'+checksum().split('-')[0]+'.jpg' : '/pics/generic.jpg' }")
+                      img(width="150",height="100",data-bind="attr:{ src: checksum() ?  '/sc/'+checksum().split('-')[0]+'.jpg' : '/pics/generic.jpg' }")
                   td
                       - if (is_playlist)
                         button.close.remove-media(type='button', data-bind='click: $root.removeItem, clickBubble: false') ×

views/templates/mediaview.jade
@@ -1,5 +1,5 @@
 - if (_id)
-  - var picture= 'sc/' + _id + '.jpg'
+  - var picture= '/sc/' + _id + '.jpg'
 - if (!description)
   - var description= i18n.gettext('Notes about this media')
 - if (!picture)

commit f53f46e00b73afe80b62ffa1449a60dcc54083ac

Fix typo introduced in df912011
Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

server.js
@@ -245,7 +245,7 @@ var utils = require('./utils');
 if (process.env.MBC_SCRAPE) {
     setTimeout(function () {
         utils.scrape_files (conf.Dirs.scrape, function (model) {
-            db.collection(collection.Medias).insert(model, {safe:true}, function(err, result) {
+            db.collection(collections.Medias).insert(model, {safe:true}, function(err, result) {
                 if (err) {
                     console.error ('error','An error has occurred' + err);
                 } else {