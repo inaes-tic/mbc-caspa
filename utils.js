5adbddb65f2ffbc1ee5d6030f90a6cf1f9f8ed45
José Luis Di Biase <josx@interorganic.com.ar>
2013-09-26 13:58:23 -0300

    Making a class with utils to reuse db connection
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- utils.js
+++ utils.js
@@ -5,14 +5,17 @@ var     _ = require('underscore')
 ,  mbc    = require('mbc-common')
 , conf    = mbc.config.Caspa
 , logger  = mbc.logger().addLogger('caspa_util')
+, collections = mbc.config.Common.Collections
 ;
 
 var _exists     = fs.exists     || require('path').exists;
 var _existsSync = fs.existsSync || require('path').existsSync;
 
-var db = mbc.db();
+var utils = module.exports = exports = function(db) {
+    this.db = db || mbc.db();
+}
 
-exports.merge = function (original_filename, callback) {
+utils.prototype.merge = function (original_filename, callback) {
     var i = 1;
     var path = require ('path');
     var source_base = path.join (conf.Dirs.uploads, '/resumable-' + original_filename + '.');
@@ -28,9 +31,10 @@ exports.merge = function (original_filename, callback) {
         writter.write (fs.readFileSync (source_base +i));
     };
 
+    var self = this;
     var id = setInterval (function () {
         logger.info('about to call merge_finish');
-        exports.merge_finish(dest, id, callback)
+        self.merge_finish(dest, id, callback)
     }, 200);
 
     return dest;
@@ -39,26 +43,28 @@ exports.merge = function (original_filename, callback) {
 /* all this hack, is because sometimes merge_finish was called before dest could be found
    this hopes to fix it */
 
-exports.merge_finish = function (dest, id, callback) {
+utils.prototype.merge_finish = function (dest, id, callback) {
     if (! _existsSync (dest))
         return;
 
     clearInterval (id);
 
     var stat = fs.statSync (dest);
-    logger.debug('all good, pasing it to parse', exports.parse_pool);
-    exports.parse_pool.task(dest, stat, function (res, err) {
+    logger.debug('all good, pasing it to parse', this.parse_pool());
+
+    var self = this;
+    this.parse_pool().task(dest, stat, function (res, err) {
         if (err)
             return (logger.error('error:', err));
-        logger.debug('parsed: ' + stat.name);
-        exports.sc_pool.task(res, callback, function (err, res) {return res});
+        logger.debug ('parsed: ' + stat.name);
+        self.sc_pool.task(res, callback, function (err, res) {return res});
     });
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 // Populate database with sample data -- Only used once: the first time the application is started.
 // You'd typically not find this code in a real-life app, since the database would already exist.
-var populateDB = function() {
+utils.prototype.populateDB = function() {
     /*
     setInterval(function () {_addMedia ({ file: 'test' + Date.now(), _id: Date.now()})},
                 4*1000);
@@ -67,12 +73,12 @@ var populateDB = function() {
     */
 }
 
-exports.sc_pool = new fp.Pool({size: 1}, function (media, callback, done) {
+utils.prototype.sc_pool = new fp.Pool({size: 1}, function (media, callback, done) {
     var dest = conf.Dirs.screenshots + '/' + media._id + '.jpg';
     logger.info('starting sc', media.file);
 /*
     if (_existsSync('./public/sc/' + media._id)) {
-        console.log ('skipping screenshot of: ' + md5 + '(file already there).');
+        logger.info ('skipping screenshot of: ' + md5 + '(file already there).');
         return done(media);
     }
 */
@@ -104,30 +110,31 @@ exports.sc_pool = new fp.Pool({size: 1}, function (media, callback, done) {
     });
 });
 
-exports.parse_pool = new fp.Pool({size: 1}, function (file, stat, done) {
-    var spawn = require('child_process').spawn,
-    md5sum    = spawn('md5sum', [file]);
+utils.prototype.parse_pool = function () {
+    var self = this;
+    return new fp.Pool({size: 1}, function (file, stat, done) {
+        var spawn = require('child_process').spawn,
+        md5sum    = spawn('md5sum', [file]);
 
-    logger.info("looking at :" + file);
-    md5sum.stdout.on('data', function (data) {
-        var md5 = data.toString().split(' ')[0];
+        logger.info("looking at :" + file);
+        md5sum.stdout.on('data', function (data) {
+            var md5 = data.toString().split(' ')[0];
 
-        db.collection('medias').findOne(md5, function(err, item) {
-            if (!err && item) {
-                if (stat === item.stat) return (done(item));
-                else item.stat = stat;
-            } else {
-                item = { _id: md5 , file: file, stat: stat};
-            }
+            self.db.collection(collections.Medias).findOne(md5, function(err, item) {
+                if (!err && item) {
+                    if (stat === item.stat) return (done(item));
+                    else item.stat = stat;
+                } else {
+                    item = { _id: md5 , file: file, stat: stat};
+                }
 
-            return done(item);
+                return done(item);
+            });
         });
-
     });
-});
+};
 
-
-exports.scrape_files = function (path, callback) {
+utils.prototype.scrape_files = function (path, callback) {
   var walk      = require('walk')
     , spawn     = require('child_process').spawn
     , bs        = 10*1024*1024
@@ -144,20 +151,23 @@ exports.scrape_files = function (path, callback) {
        + when all is done and good, we _addMedia, to get it into the medias objects;
     */
 
+    var self = this;
     //This listens for files found
     walk.walk(observe, { followLinks: false })
     .on('file', function (root, stat, next) {
         var file = root + '/' +  stat.name;
         next();
         if (! stat.name.match(/\.(webm|mp4|flv|avi|mpeg|mpeg2|mpg|mov|mkv|ogm|ogg)$/i)) {
-            return new Error('file not a vid');
+            var error = new Error('file not a vid');
+            logger.error('ERROR', error);
+            return error;
         }
 
-        exports.parse_pool.task(file, stat, function (res, err) {
+        self.parse_pool().task(file, stat, function (res, err) {
             if (err)
                 return (logger.error('error:', err));
             logger.debug('parsed: ' + stat.name, res);
-            exports.sc_pool.task(res, callback, function (err, res) {return res});
+            self.sc_pool.task(res, callback, function (err, res) {return res});
         });
 
     })
@@ -166,7 +176,8 @@ exports.scrape_files = function (path, callback) {
     });
 }
 
-exports.check_media = function (media, cb, arg) {
+utils.prototype.check_media = function (media, cb, arg) {
+    var self = this;
     if (!arg)
         arg = media;
 
@@ -177,7 +188,7 @@ exports.check_media = function (media, cb, arg) {
             cb(arg)
         _exists (conf.Dirs.screenshots + '/' + media._id + '.jpg', function (e) {
             if (!e)
-                exports.sc_pool.task (media, null, function (res, err) {
+                self.sc_pool.task (media, null, function (res, err) {
                     if (err) {
                         logger.error(new Error("couldn't sc"));
                     }
