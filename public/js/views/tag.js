6534889a14d6b0d070db30ec6ecc075491b13d3a
José Luis Di Biase <josx@interorganic.com.ar>
2013-11-22 15:34:49 -0300

    Adding Autocomplete hightlighted search
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- public/js/views/tag.js
+++ public/js/views/tag.js
@@ -2,6 +2,8 @@ window.TagTransformView = function (options) {
     var self = this;
     options = options || {};
     var tagCollection;
+    var searchTagCollection = new Media.TagCollectionPageable();
+    var page_size = options['page_size'] || (searchTagCollection instanceof PageableCollection)?searchTagCollection.state.pageSize:10;
     var model = options['model'];
     var el = options['el'] || $('#content');
     this.el = el;
@@ -19,12 +21,14 @@ window.TagTransformView = function (options) {
 
     el.html(template.tag({type: type}));
 
-    var TagItemViewModel = kb.ViewModel.extend({
-        constructor: function(model) {
-            var self = this;
-            kb.ViewModel.prototype.constructor.apply(this, arguments);
-        }
-    });
+    var TagItemViewModel = function(id, name, color) {
+            this.id = ko.observable(id);
+            this.name = ko.observable(name);
+            this.color = ko.observable(color);
+            this.displayName = ko.dependentObservable(function() {
+                return this.name() + " [" + this.color() + "]";
+            }, this);
+    };
 
     var TagsViewModel = kb.ViewModel.extend({
         constructor: function() {
@@ -32,7 +36,6 @@ window.TagTransformView = function (options) {
             var self = this;
 
             this.tags =  kb.collectionObservable(tagCollection, {
-                view_model: TagItemViewModel,
             });
 
             this.removeRelatedTag =  function (vm) {
@@ -97,6 +100,24 @@ window.TagTransformView = function (options) {
         clearAddForm: function () {
             this.tagName('');
             this.tagColor('');
+        },
+        tagsName: ko.observableArray(),
+        selectedTagId: ko.observable(),
+        getServerTags:  function (searchTerm, sourceArray) {
+            var result = [];
+            query_obj = { text : searchTerm };
+            searchTagCollection.setQuery(query_obj, page_size);
+            searchTagCollection.fetch({
+                success: function() {
+                    _.map(
+                        searchTagCollection.toJSON(),
+                        function(model) {
+                            result.push(new TagItemViewModel(model._id, model.name, model.color));
+                        }
+                    );
+                    sourceArray(result);
+                }
+            });
         },
     });
 
@@ -225,5 +246,130 @@ window.TagView = function(options){
         this.destroyView();
         options["ok"]();
     };
+};
 
+//jqAuto -- main binding (should contain additional options to pass to autocomplete)
+//jqAutoSource -- the array to populate with choices (needs to be an observableArray)
+//jqAutoQuery -- function to return choices
+//jqAutoValue -- where to write the selected value
+//jqAutoSourceLabel -- the property that should be displayed in the possible choices
+//jqAutoSourceInputValue -- the property that should be displayed in the input box
+//jqAutoSourceValue -- the property to use for the value
+//jqHighLightClass
+ko.bindingHandlers.jqAuto = {
+    init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+        var options = valueAccessor() || {},
+            allBindings = allBindingsAccessor(),
+            unwrap = ko.utils.unwrapObservable,
+            modelValue = allBindings.jqAutoValue,
+            source = allBindings.jqAutoSource,
+            query = allBindings.jqAutoQuery,
+            valueProp = allBindings.jqAutoSourceValue,
+            inputValueProp = allBindings.jqAutoSourceInputValue || valueProp,
+            labelProp = allBindings.jqAutoSourceLabel || inputValueProp,
+            highLightClass = allBindings.jqHighLightClass || 'ui-state-highlight';
+
+        //function that is shared by both select and change event handlers
+        function writeValueToModel(valueToWrite) {
+            if (ko.isWriteableObservable(modelValue)) {
+               modelValue(valueToWrite );
+            } else {  //write to non-observable
+               if (allBindings['_ko_property_writers'] && allBindings['_ko_property_writers']['jqAutoValue'])
+                        allBindings['_ko_property_writers']['jqAutoValue'](valueToWrite );
+            }
+        }
+
+        //on a selection write the proper value to the model
+        options.select = function(event, ui) {
+            writeValueToModel(ui.item ? ui.item.actualValue : null);
+        };
+
+        //on a change, make sure that it is a valid value or clear out the model value
+        options.change = function(event, ui) {
+            var currentValue = $(element).val();
+            var matchingItem =  ko.utils.arrayFirst(unwrap(source), function(item) {
+               return unwrap(inputValueProp ? item[inputValueProp] : item) === currentValue;
+            });
+
+            if (!matchingItem) {
+               writeValueToModel(null);
+            }
+        }
+
+        //hold the autocomplete current response
+        var currentResponse = null;
+
+        //handle the choices being updated in a DO, to decouple value updates from source (options) updates
+        var mappedSource = ko.dependentObservable({
+            read: function() {
+                    mapped = ko.utils.arrayMap(unwrap(source), function(item) {
+                        var result = {};
+                        result.label = labelProp ? unwrap(item[labelProp]) : unwrap(item).toString();  //show in pop-up choices
+                        result.value = inputValueProp ? unwrap(item[inputValueProp]) : unwrap(item).toString();  //show in input box
+                        result.actualValue = valueProp ? unwrap(item[valueProp]) : item;  //store in model
+                        return result;
+                });
+                return mapped;
+            },
+            write: function(newValue) {
+                source(newValue);  //update the source observableArray, so our mapped value (above) is correct
+                if (currentResponse) {
+                    currentResponse(mappedSource());
+                }
+            },
+            disposeWhenNodeIsRemoved: element
+        });
+
+        if (query) {
+            options.source = function(request, response) {
+                currentResponse = response;
+                query.call(this, request.term, mappedSource);
+            }
+        } else {
+            //whenever the items that make up the source are updated, make sure that autocomplete knows it
+            mappedSource.subscribe(function(newValue) {
+               $(element).autocomplete("option", "source", newValue);
+            });
+
+            options.source = mappedSource();
+        }
+
+        //initialize autocomplete
+        $(element).autocomplete(options).data('autocomplete')._renderItem = function( ul, item ) {
+            // Replace the matched text with a custom span. This
+            // span uses the class found in the "highlightClass" option.
+            var re = new RegExp( "(" + this.term + ")", "gi" ),
+                cls = highLightClass,
+                template = "<span class='" + cls + "'>$1</span>",
+                label = item.label.replace( re, template ),
+                $li = $( "<li/>" ).appendTo( ul );
+
+            // Create and return the custom menu item content.
+            $( "<a/>" ).attr( "href", "#" )
+                       .html( label )
+                       .appendTo( $li );
+            return $li;
+        };
+
+
+    },
+    update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+       //update value based on a model change
+       var allBindings = allBindingsAccessor(),
+           unwrap = ko.utils.unwrapObservable,
+           modelValue = unwrap(allBindings.jqAutoValue) || '',
+           valueProp = allBindings.jqAutoSourceValue,
+           inputValueProp = allBindings.jqAutoSourceInputValue || valueProp;
+
+       //if we are writing a different property to the input than we are writing to the model, then locate the object
+       if (valueProp && inputValueProp !== valueProp) {
+           var source = unwrap(allBindings.jqAutoSource) || [];
+           var modelValue = ko.utils.arrayFirst(source, function(item) {
+                 return unwrap(item[valueProp]) === modelValue;
+           }) || {};
+       }
+
+       //update the element with the value that should be shown in the input
+       $(element).val(modelValue && inputValueProp !== valueProp ? unwrap(modelValue[inputValueProp]) : modelValue.toString());
+    }
 };
