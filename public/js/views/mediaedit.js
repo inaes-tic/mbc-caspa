0cfd74b9cc0be7817946d568267d83415476fa43
Adrian Pardini <publico@tangopardo.com.ar>
2014-01-27 15:33:04 -0300

    EditView: make savePlaylist() work with the reverse relationship from Piece to Playlist.
    In models/Media.js we set up the reverse relation from Piece to Playlist
    so when a new Piece is created somewhere else it doesn't end up tied to
    a random playlist on the other browsers as it did before.
    
    This works like a charm for an already existing playlist.
    
    However, if we are about to save a new playlist it doesn't have an _id
    yet, and so all the pieces are left with a null parentPlaylist.
    This ultimately leads to them and the original playlist not being saved.
    Well, it's worse than that. They are saved but parentPlaylist is null and on
    the playlist 'pieces' is an array of null.
    
    So we first save the playlist without pieces, the server emits a 'create'
    event and notifies everyone.
    
    After that, we have a playlist with an id. Then we re-add all the pieces and
    save (again). This will correctly create the pieces and relations here and on
    the other browsers.
    
    Why not just slap an uuid.v4() like the middleware would do anyway?
    If we do that the call to isNew() returns false. Here it's no biggie but the
    fine oiled machine that our Backbone-* stack is uses it internally, and I don't
    really feel like messing with that. One of the most visible consecuences of it
    is that the server broadcasts an 'update' event instead of a 'create', so the
    other clients won't see the new playlist.
    
    Nowadays we are only creating playlists from here but all this sholud really go
    inside the model. But there we don't always have a Universe (for new playlists
    this.collection is undefined)
    
    Sorry everyone for this. All I can say is that I am grateful we dont
    have yet means to punch someone over the net.

--- public/js/views/mediaedit.js
+++ public/js/views/mediaedit.js
@@ -131,36 +131,91 @@ window.EditView = PanelView.extend({
             return;
         }
 
-        pieces.bind("sync", function() {
-            if (_.every(pieces.pluck("_id"))) {
-                // All pieces are stored in the DB
-                pieces.off("sync");
-                if (col.isNew()) {
-                    console.log ("about to feed this to the universe:", col.attributes);
-                    self.collection.create(col, {
-                        success: function(mod) {
-                            // this is called after we get the model just created with an id from the server,
-                            // so we can update the view with the new model.
-                            // WHY not just use the result of collection.create()?
-                            // even if we set {wait: true} that model won't have an id, at least not after the 'sync'
-                            // event fires when we get the response from the server, so calling .save() on that leads
-                            // to duplicate (but different) playlist and all sorts of fun debugging time.
-                            self.editview.save(mod);
-                        },
-                    });
-                    console.log('WE HAVE ADDED TO THE UNIVERSE', col);
-                } else {
-                    // if we are paginating Universe the model being edited can no longer be inside Universe
-                    // so calling .save() on that fails. But, calling .create() and passing an existing model
-                    // makes it to the server, updates our Universe and restores relationships. Sometimes we
-                    // can have nice things after all.
-                    self.collection.create(col);
-                    console.log ('universe knows of us, just saving');
-                }
-            }
-        });
+        /*
+         * Fun hacking time.
+         * In models/Media.js we set up the reverse relation from Piece to Playlist
+         * so when a new Piece is created somewhere else it doesn't end up tied to
+         * a random playlist on the other browsers as it did before.
+         *
+         * This works like a charm for an already existing playlist.
+         *
+         * However, if we are about to save a new playlist it doesn't have an _id
+         * yet, and so all the pieces are left with a null parentPlaylist.
+         * This ultimately leads to them and the original playlist not being saved.
+         * Well, it's worse than that. They are saved but parentPlaylist is null and on
+         * the playlist 'pieces' is an array of null.
+         *
+         * So we first save the playlist without pieces, the server emits a 'create'
+         * event and notifies everyone.
+         *
+         * After that, we have a playlist with an id. Then we re-add all the pieces and
+         * save (again). This will correctly create the pieces and relations here and on
+         * the other browsers.
+         *
+         * Why not just slap an uuid.v4() like the middleware would do anyway?
+         * If we do that the call to isNew() returns false. Here it's no biggie but the
+         * fine oiled machine that our Backbone-* stack is uses it internally, and I don't
+         * really feel like messing with that. One of the most visible consecuences of it
+         * is that the server broadcasts an 'update' event instead of a 'create', so the
+         * other clients won't see the new playlist.
+         *
+         * Nowadays we are only creating playlists from here but all this sholud really go
+         * inside the model. But there we don't always have a Universe (for new playlists
+         * this.collection is undefined)
+         */
 
-        pieces.invoke("save");
+        if (col.isNew()) {
+            var orphans = _.clone(pieces.models); // It's okay as it is a shallow copy.
+            self.collection.create(col, {
+                success: function(mod) {
+                    mod.get('pieces').add(orphans);;
+                    save_model_pieces(mod);
+                },
+            });
+        } else {
+            save_model_pieces();
+        }
+
+        function save_model_pieces(mod) {
+            // col is defined above.
+            col = mod || col;
+            var isNew = (mod != undefined);
+            var pieces = col.get('pieces');
+            pieces.bind("sync", function() {
+                if (_.every(pieces.pluck("_id"))) {
+                    // All pieces are stored in the DB
+                    pieces.off("sync");
+                    if (isNew) {
+                        console.log ("about to feed this to the universe:", col.attributes);
+                        self.collection.create(col, {
+                            success: function(mod) {
+                                /*
+                                 * this is called after we get the model just created with an id from the server,
+                                 * so we can update the view with the new model.
+                                 * WHY not just use the result of collection.create()?
+                                 * even if we set {wait: true} that model won't have an id, at least not after the 'sync'
+                                 * event fires when we get the response from the server, so calling .save() on that leads
+                                 * to duplicate (but different) playlist and all sorts of fun debugging time.
+                                 */
+                                self.editview.save(mod);
+                            },
+                        });
+                        console.log('WE HAVE ADDED TO THE UNIVERSE', col);
+                    } else {
+                        /*
+                         * if we are paginating Universe the model being edited can no longer be inside Universe
+                         * so calling .save() on that fails. But, calling .create() and passing an existing model
+                         * makes it to the server, updates our Universe and restores relationships. Sometimes we
+                         * can have nice things after all.
+                         */
+                        self.collection.create(col);
+                        console.log ('universe knows of us, just saving');
+                    }
+                }
+            });
+            pieces.invoke("save");
+        }
+
 
         this.editview.clearChanges();
         this.delegateEvents();
