22f3002f5390825fbee186b53f06c274e76df306
Leonardo Vidarte <lvidarte@gmail.com>
2014-02-14 00:44:26 -0300

    Added mosto messages

--- public/js/views/header.js
+++ public/js/views/header.js
@@ -18,6 +18,104 @@ window.TimeInfoView = Backbone.View.extend({
     }
 });
 
+window.MostoMessagesViewModel = function(collection) {
+    var self = this;
+
+    this.radioValue = ko.observable("");
+
+    this._collection = collection;
+
+    this.collection = kb.collectionObservable(collection, {
+        filters: function(model) {
+            return model.get('type') === self.radioValue();
+        }
+    });
+
+    //-----
+    this.collectionNoFilter = kb.collectionObservable(collection);
+
+    this.count = ko.computed(function() {
+        return self.collectionNoFilter().length;
+    });
+    //-----
+
+    this.countClass = ko.computed(function() {
+        var class_ = 'notification';
+        _.each(self.collectionNoFilter(), function(viewModel) {
+            if (viewModel.model().isError()) {
+                class_ = 'error';
+            }
+        });
+        return class_;
+    });
+
+    this.allClass = ko.computed(function() {
+        return self.radioValue() == '' ? 'all-enabled' : 'disabled';
+    });
+
+    this.notificationsClass = ko.computed(function() {
+        return self.radioValue() == 'error' ? 'notifications-enabled' : 'disabled';
+    });
+
+    this.errorsClass = ko.computed(function() {
+        return self.radioValue() == 'notification' ? 'errors-enabled' : 'disabled';
+    });
+
+    this.hasNotifications = ko.computed(function() {
+        var found = false;
+        _.each(self.collection(), function(viewModel) {
+            if (viewModel.model().isNotification()) {
+                found = true;
+            }
+        });
+        return found;
+    });
+
+    this.remove = function(viewModel) { 
+        var model = viewModel.model();
+        this._collection.remove(model);
+    };
+
+    this.removeAll = function() {
+        var models = $.extend({}, self._collection.models);
+        _.each(models, function(model) {
+            if (model.isNotification()) {
+                self._collection.remove(model);
+            }
+        })
+    };
+}
+
+window.mostoMessages = new App.MostoMessages([
+    // for testing
+    new App.MostoMessage({title: 'Title for notification', message: 'Text body notification text'}),
+    new App.MostoMessage({title: 'Notification title', message: 'Sample notification text'}),
+    new App.MostoMessage({title: 'Example error title', message: 'Description error sample', type: 'error'}),
+    new App.MostoMessage({title: 'Sample notification', message: 'Text sample notification'})
+]);
+
+window.mostoMessagesViewModel = new MostoMessagesViewModel(mostoMessages);
+
+// for testing
+window.addMostoMessage = function(type) {
+    mostoMessages.add(new App.MostoMessage({
+        type: type,
+        title: "Title for new " + type,
+        message: "message at " + (new Date()).toLocaleString()}
+    ));
+};
+
+// for testing
+window.removeErrorMostoMessages = function() {
+    var models = $.extend({}, mostoMessages.models);
+    _.each(models, function(model) {
+        if (model.isError()) {
+            mostoMessages.remove(model);
+        }
+    });
+};
+//*/
+
 window.HeaderView = function (options) {
 
     var self = this;
@@ -53,10 +151,10 @@ window.HeaderView = function (options) {
         }
     });
 
-
     self.view_model = {
         Header:new HeaderViewModel(self.appstatus),
         Frame: new FrameViewModel(self.framestatus),
+        Messages: mostoMessagesViewModel,
     };
 
     ko.applyBindings(self.view_model, self.el[0]);
@@ -88,5 +186,9 @@ window.HeaderView = function (options) {
         $(window).resize();
     };
     $('#toggle-header').click(toggle);
+    $('#mosto-messages label').on('click', function(e) { e.stopPropagation(); });
+    $('#mosto-messages input').on('click', function(e) { e.stopPropagation(); });
+    $('#mosto-messages a').on('click', function(e) { e.stopPropagation(); });
+
 }
 
